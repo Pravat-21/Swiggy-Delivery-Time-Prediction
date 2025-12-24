3c4ab186f7081a19f8c2177be705040236457cb2
Pravat Patra <pravatpatra1997@gmail.com>
2025-12-24 18:58:37 +0530

    creating version-2 of model by changing parameters values in params.yaml

---
+++ app.py
@@ -0,0 +1,158 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from sklearn.pipeline import Pipeline
+import uvicorn
+import pandas as pd
+import mlflow
+import json
+import pickle
+from mlflow import MlflowClient
+from fastapi import FastAPI, Request, UploadFile, File
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from sklearn import set_config
+import dagshub
+from src.exception import CustomException
+import sys
+from pathlib import Path
+from src.logger import file_logging, console_logging
+from src.utils import load_processor,basic_test_data_cleaning
+#=============================================================================
+#=======================================================================================================
+file_logger = file_logging("Model_register_file")
+console_logger = console_logging("Model_register_console")
+#=======================================================================================================
+dagshub.init(repo_owner='Pravat-21', repo_name='Swiggy-Delivery-Time-Prediction', mlflow=True)
+mlflow.set_tracking_uri("https://dagshub.com/Pravat-21/Swiggy-Delivery-Time-Prediction.mlflow")
+#=======================================================================================================
+
+class Data(BaseModel):  
+    ID: str
+    Delivery_person_ID: str
+    Delivery_person_Age: str
+    Delivery_person_Ratings: str
+    Restaurant_latitude: float
+    Restaurant_longitude: float
+    Delivery_location_latitude: float
+    Delivery_location_longitude: float
+    Order_Date: str
+    Time_Orderd: str
+    Time_Order_picked: str
+    Weatherconditions: str
+    Road_traffic_density: str
+    Vehicle_condition: int
+    Type_of_order: str
+    Type_of_vehicle: str
+    multiple_deliveries: str
+    Festival: str
+    City: str
+
+def load_model_info(file_path: str) -> dict:
+    """Load the model info from a JSON file."""
+    try:
+        with open(file_path, 'r') as file:
+            model_info = json.load(file)
+
+        file_logger.debug(f'Model info loaded from {file_path}')
+        console_logger.debug(f'Model info loaded from {file_path}')
+
+        return model_info
+    except FileNotFoundError as f:
+        file_logger.error(f'File not found: {file_path} & error is: {f}')
+        console_logger.error(f'File not found: {file_path}& error is: {f}')
+        raise CustomException(f,sys)
+    except Exception as e:
+        file_logger.error(f'Unexpected error occurred while loading the model info: {e}')
+        console_logger.error(f'Unexpected error occurred while loading the model info: {e}')
+        raise CustomException(e,sys)
+    
+def get_latest_model_version(model_name):
+    """This function helps us to get the latest version of model from model registry"""
+
+    try:
+        client = mlflow.MlflowClient()
+        latest_version = client.get_latest_versions(model_name, stages=["Production"])
+        if not latest_version:
+            latest_version = client.get_latest_versions(model_name, stages=["Staging"])
+        if not latest_version:
+            latest_version = client.get_latest_versions(model_name, stages=["None"])
+        
+        file_logger.info("Latest version of model has been fetched")
+        console_logger.info("Latest version of model has been fetched")
+
+        return latest_version[0].version if latest_version else None
+    
+    except Exception as e:
+        file_logger.error(f'Unexpected error occurred while get_latest_model_version: {e}')
+        console_logger.error(f'Unexpected error occurred while get_latest_model_version: {e}')
+        raise CustomException(e,sys)
+    
+root = Path(__file__).parent
+
+processor_path = root/"models"/"processor.pkl"
+
+processor = load_processor(processor_path)
+
+model_name = "swiggy_regressor"
+model_version = get_latest_model_version(model_name)
+model_stage = "Staging"
+print(model_version)
+model_uri = f"models:/{model_name}/{model_version}"
+#model = mlflow.sklearn.load_model(model_uri)
+model = mlflow.pyfunc.load_model(model_uri)
+
+model_pipe = Pipeline(steps=[
+('preprocess',processor),
+("regressor",model)
+])
+    
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Hello, welcome to swiggy time prediciton page"
+
+@app.post(path="/predict")
+def do_predictions(data: Data):
+    pred_data = pd.DataFrame({
+        'ID': data.ID,
+        'Delivery_person_ID': data.Delivery_person_ID,
+        'Delivery_person_Age': data.Delivery_person_Age,
+        'Delivery_person_Ratings': data.Delivery_person_Ratings,
+        'Restaurant_latitude': data.Restaurant_latitude,
+        'Restaurant_longitude': data.Restaurant_longitude,
+        'Delivery_location_latitude': data.Delivery_location_latitude,
+        'Delivery_location_longitude': data.Delivery_location_longitude,
+        'Order_Date': data.Order_Date,
+        'Time_Orderd': data.Time_Orderd,
+        'Time_Order_picked': data.Time_Order_picked,
+        'Weatherconditions': data.Weatherconditions,
+        'Road_traffic_density': data.Road_traffic_density,
+        'Vehicle_condition': data.Vehicle_condition,
+        'Type_of_order': data.Type_of_order,
+        'Type_of_vehicle': data.Type_of_vehicle,
+        'multiple_deliveries': data.multiple_deliveries,
+        'Festival': data.Festival,
+        'City': data.City
+        },index=[0]
+    )
+
+    cleaned_data = basic_test_data_cleaning(pred_data)
+    df =cleaned_data.dropna()
+
+    
+
+    predictions = model_pipe.predict(df)[0]
+    return predictions
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="app:app",host="0.0.0.0",port=8000)
+
+    
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -8,9 +8,121 @@ from src.logger import file_logging, console_logging
 from src.exception import CustomException
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator as Model
+import haversine as hs
 
 file_logger = file_logging("src_Utils_file")
 con_logger = console_logging("src_Utils_console")
+
+def time_diff(row):
+    try:
+        #logic of function
+        order_picked = pd.to_timedelta(row['Time_Order_picked'])
+        time_order =  pd.to_timedelta(row['Time_Orderd'])
+        x = order_picked - time_order
+        return x
+    
+    except Exception as e:
+        file_logger.error(f"Error has been occured into time_diff function in utils & the error is {e}")
+        con_logger.error(f"Error has been occured into time_diff function in utils& the error is {e}")
+        raise CustomException(e,sys)
+
+def haversine_row(row):
+    try:
+        # logic of function
+        loc1 = (row['Restaurant_latitude'], row['Restaurant_longitude'])
+        loc2 = (row['Delivery_location_latitude'], row['Delivery_location_longitude'])
+        hvr_distance = hs.haversine(loc1, loc2, unit=hs.Unit.KILOMETERS)
+        return hvr_distance
+    
+    except Exception as e:
+        file_logger.error(f"Error has been occured into haversine_row function in utils & the error is {e}")
+        con_logger.error(f"Error has been occured into haversine_row function in utils & the error is {e}")
+        raise CustomException(e,sys)
+
+
+def basic_test_data_cleaning(df):
+    try:
+        file_logger.info("Creating basic_test_data_cleaning function in utils.....")
+        con_logger.info("Creating basic_test_data_cleaning function in utils.....")
+
+        # logic of the function
+        df.drop(columns=['ID'],inplace=True)
+        df['Restaurant_latitude'] = abs(df['Restaurant_latitude'].replace(0,np.nan))
+        df['Restaurant_longitude'] = abs(df['Restaurant_longitude'].replace(0,np.nan))
+        cols = ["Delivery_location_latitude","Delivery_location_longitude"]
+        df.loc[df['Restaurant_latitude'].isnull(), cols] = np.nan
+
+        columns = ["Delivery_person_Age",  "Delivery_person_Ratings","Time_Orderd", "Weatherconditions", "Road_traffic_density",
+        "multiple_deliveries", "Festival", "City"]
+        for i in columns:
+            df[i] = df[i].replace('NaN ',np.nan)
+        df['Weatherconditions'] = df['Weatherconditions'].replace("conditions NaN",np.nan)
+        df['Weatherconditions'] = df['Weatherconditions'].apply(lambda x: x.split()[-1] if isinstance(x, str) else x)
+        #df['Time_taken(min)'] = df['Time_taken(min)'].apply(lambda x: x[-2:]).astype("float")
+
+        # changing datatype 
+        df['Delivery_person_Age'] = df['Delivery_person_Age'].astype("float")
+        df['Delivery_person_Ratings'] = df['Delivery_person_Ratings'].astype("float")
+        df['Order_Date'] = pd.to_datetime(df['Order_Date'])
+        df['multiple_deliveries'] = df['multiple_deliveries'].astype("float")
+
+        # applying harversine distance
+        df['Distance_res_to_loc_KM'] = df[['Restaurant_latitude', 'Restaurant_longitude','Delivery_location_latitude', 'Delivery_location_longitude']].apply(haversine_row,axis=1)
+        df.insert(7,"Distance_res_loc_KM",df.pop('Distance_res_to_loc_KM'))
+
+        # finding the time taken between restrurant & order pickup
+        df["Time_res_pickup"] = (df[['Time_Order_picked','Time_Orderd']].apply(time_diff,axis=1).dt.total_seconds()/60).replace({-1425.:15., -1430.:10., -1435.:5.})
+        df.insert(11,"Time_res_pickup",df.pop("Time_res_pickup"))
+        df["order_time_hr"] = pd.to_datetime(df['Time_Orderd']).dt.hour
+        df.insert(10,"order_time_hr",df.pop("order_time_hr"))
+        df["time_of_day"] = pd.cut(
+        df['order_time_hr'],
+        bins=[0, 6, 12, 17, 20, 24],
+        labels=['after midnight', 'morning', 'afternoon', 'evening', 'night'],
+        right=False
+        )
+        df.insert(11,"time_of_day",df.pop("time_of_day"))
+
+        # fixing values in columns:
+        df['Road_traffic_density'] = df['Road_traffic_density'].str.strip().str.lower()
+        df['Weatherconditions'] = df['Weatherconditions'].str.strip().str.lower()
+        df['Type_of_order'] = df['Type_of_order'].str.strip().str.lower()
+        df['Type_of_vehicle'] = df['Type_of_vehicle'].str.strip().str.lower()
+        df['Festival'] = df['Festival'].str.strip().str.lower()
+        df['City'] = df['City'].str.strip().str.lower()
+
+        # dropping some rows
+        df.drop(index=df[df['Delivery_person_Age'] <18].index,inplace=True)
+        df.drop(index=df[df['Delivery_person_Ratings']>5].index,inplace=True)
+        
+        # creating rider city info
+        rename_city = {"INDO":"Indore","BANG":"Bengaluru","COIMB":"Coimbatore","CHEN":"Chennai","HYD":"Hyderabad",
+                "RANCHI":"Ranchi","MYS":"Mysore","DEH":"Dehradun","KOC":"Kochi","PUNE":"Pune","LUD":"Ludhiana",
+                "LUDH":"Ludhiana","KNP":"Kanpur","MUM":"Mumbai","KOL":"Kolkata","JAP":"Jamshedpur","SUR":"Surat",
+                "GOA":"Goa","AURG":"Aurangabad","AGR":"Agra","VAD":"Vadodara","ALH":"Allahabad","BHP":"Bhopal"}
+        
+        df['rider_city'] = df['Delivery_person_ID'].str.split("RES").str.get(0).replace(rename_city)
+        df.insert(1,'rider_city',df.pop('rider_city'))
+        df.insert(11,'order_dayname',df['Order_Date'].dt.day_name())
+        df.insert(12,'order_in_weekend',np.where(df['order_dayname'].isin(['Saturday','Sunday']),1,0))
+        
+
+        df.drop(columns=['Delivery_person_ID'],inplace=True)
+
+        # after doing EDA these columns are selected to be removed
+        df.drop(columns=['rider_city','Type_of_order','order_in_weekend','order_time_hr','Time_res_pickup','Restaurant_latitude','Restaurant_longitude','Delivery_location_latitude','Delivery_location_longitude','Order_Date','Time_Orderd','Time_Order_picked'],inplace=True)
+
+        df = df.dropna()
+
+        file_logger.info("Successfully created basic_test_data_cleaning function.")
+        con_logger.info("Successfully created basic_test_data_cleaning function.")
+        
+        return df
+    
+    except Exception as e:
+        file_logger.error(f"Error has been occured into basic_test_data_cleaning function in utils & the error is {e}")
+        con_logger.error(f"Error has been occured into basic_test_data_cleaning function in utils & the error is {e}")
+        raise CustomException(e,sys)
 
 
 def save_data(df:pd.DataFrame, path:str)-> None:
@@ -108,16 +220,30 @@ def save_model(model:Model, path:str)->None:
 def load_model(file_path: str)->Model:
     """Load the trained model from a file."""
     
-    file_logger.info("In load_model function from model_evaluation module....")
-    con_logger.info("In load_model function from model_evaluation module....")
-
     try:
         with open(file_path, 'rb') as file:
             model = pickle.load(file)
 
         file_logger.info(f'Model loaded from {file_path}')
+        con_logger.info(f'Model loaded from {file_path}')
         return model
     except Exception as e:
-        file_logger.error(f"In load_model function from load_model(), error has been ocurred & error is {e}")
+        file_logger.error(f"In load_model() from utils.py, error has been ocurred & error is {e}")
+        con_logger.error(f"In  load_model() from utils.py, error has been ocurred & error is {e}")
+        raise CustomException(e,sys)
+
+def load_processor(file_path: str)->Pipeline:
+    """Load the processor for transformation from a file."""
+
+    try:
+        with open(file_path, 'rb') as file:
+            processor = pickle.load(file)
+
+        file_logger.info(f'processor loaded from {file_path}')
+        con_logger.info(f'processor loaded from {file_path}')
+        return processor
+    except Exception as e:
+        file_logger.error(f"In utils.py, in load_processor() error has been ocurred & error is {e}")
+        con_logger.error(f"In utils.py, in load_processor() error has been ocurred & error is {e}")
         raise CustomException(e,sys)
 
